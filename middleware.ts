cf20715a1c7e7567bd5895025b09d4b4059921bb
DavidGir <davidgiroux149@gmail.com>
2024-04-04 14:56:43 -0400

    Add clerk auth middleware and config - necessary for auth setup

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: ['/'],
+});
+// Config obtained from Clerk's Next.js SDK documentation:
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
+};
