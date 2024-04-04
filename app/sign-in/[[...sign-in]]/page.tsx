ca6448b4c5615b36da6c08e34f2212ec932d99f2
DavidGir <davidgiroux149@gmail.com>
2024-04-04 15:02:15 -0400

    Add sign-in page component

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,19 @@
+import { SignIn } from '@clerk/nextjs';
+
+export default function Page() {
+  return (
+    <div className="mt-5">
+      <div className="flex mx-auto justify-center items-center mt-10">
+        <SignIn
+          appearance={{
+            elements: {
+              formButtonPrimary:
+                'bg-black hover:bg-gray-700 transition text-sm normal-case',
+            },
+          }}
+          afterSignInUrl="/dashboard"
+        />
+      </div>
+    </div>
+  );
+}
