a72311ca8f7b0830fc39d2b435c97d5396d0b152
DavidGir <davidgiroux149@gmail.com>
2024-04-05 01:22:36 -0400

    Add configuration for remote images, enable strict mode, and modify webpack externals

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        port: '',
+        pathname: '/*',
+      },
+    ],
+  },
+  experimental: {
+    esmExternals: 'loose',
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: 'canvas' }];
+    return config;
+  },
+};
 
 module.exports = nextConfig;
