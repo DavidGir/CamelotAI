0f2cf4cc80ec982d16426573e8319e1a84c7f11a
DavidGir <davidgiroux149@gmail.com>
2024-04-06 20:04:25 -0400

    Update UploadDropzone to UploadButton and handle file upload completion

--- app/dashboard/Dashboard.tsx
+++ app/dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { UploadDropzone } from "@bytescale/upload-widget-react";
+import { UploadButton } from "@bytescale/upload-widget-react";
 import { useRouter } from 'next/navigation';
 import DocumentIcon from '../../components/ui/DocumentIcon'
 import { formatDistanceToNow } from 'date-fns';
@@ -10,6 +10,7 @@ import DeleteBinIcon from "@/components/ui/DeleteBinIcon";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { SpinnerDotted } from 'spinners-react';
+import '../../styles/global.css';
 
 // Interface for the document object:
 interface Document {
@@ -34,58 +35,70 @@ export default function Dashboard({
   const [loading, setLoading] = useState(false);
   const [deletionStatus, setDeletionStatus] = useState<DeletionStatus>({});
 
-
+  // Options is an object that contains the configuration for the Upload Widget:
   const options = {
     apiKey: !!process.env.NEXT_PUBLIC_BYTESCALE_API_KEY
       ? process.env.NEXT_PUBLIC_BYTESCALE_API_KEY
       : 'No Bytescale api key found',
-    maxFileCount: 1,
+    maxFileCount: 4,
     mimeTypes: ['application/pdf'],
-    // layout: "modal",
-    
     editor: { images: { crop: false } },
     styles: {
       colors: {
-        primary: "#000000", // Primary buttons & links
-        error: "#d23f4d", // Error messages
+        primary: "#000000",   // Primary buttons & links
+        error: "#d23f4d",     // Error messages
+        shade600: "#000000",  // Border
+        shade500: "#000000",  // Modal close button
+        shade900: "#F3DFC1"   // Various (draggable crop buttons, etc.)
       },
       breakpoints: {
         fullScreenHeight: 420,
         fullScreenWidth: 750
       },
     },
-    onValidate: async (file: File): Promise<undefined | string> => {
-      if (docsList.length >= 4) {
-        const errorMessage = 'You have reached the limit of four documents!';
-        toast.error(errorMessage);
-        return errorMessage;
-      }
-      return undefined;
-    },
   };
 
-  const UploadDropZone = () => (
-    <UploadDropzone
-      options={options}
-      onUpdate={({ uploadedFiles }) => {
-        // Check if there is a file and the doc limit hasn't been reached:
-        if (uploadedFiles.length !== 0 && docsList.length < 4) {
-          setLoading(true);
-          ingestPdf(
-            uploadedFiles[0].fileUrl,
-            uploadedFiles[0].originalFile.originalFileName || uploadedFiles[0].filePath,
-          );
-        } else {
-          // When limit is reached avoid loading spinner:
-          setLoading(false);
-        }
-      }}
-      width="450px"
-      height="200px"
-      className="upload-dropzone-text upload-widget upload-widget__internal--draggable"
-    />
-  );
+  // Function to handle the completion of the upload process within the Bytescale widget:
+  const handleComplete = (files: any) => {
+    if (docsList.length + files.length > 4) {
+      toast.error('You cannot upload more than four documents.');
+      return;
+    }
+    // Create a set to track unique file names in the current batch
+    const fileNamesInBatch = new Set();
+    // Filter out the files that are already uploaded:
+    const newFiles = files.filter((file: any) => {
+      const fileName = file.originalFile.originalFileName;
+      if (fileNamesInBatch.has(fileName)) {
+        return false;
+      } else {
+        fileNamesInBatch.add(fileName);
+        return !docsList.some(doc => doc.fileName === fileName);
+      }
+    });
+      
+    // Notify the user if there are duplicates:
+    if (newFiles.length < files.length) {
+      toast.warn('Duplicate documents are not uploaded.');
+    }
 
+    if (newFiles.length === 0) {
+      setLoading(false);
+      return;
+    }
+
+    setLoading(true);
+    // Ingest the PDFs:
+    Promise.all(newFiles.map((file: any) => 
+      ingestPdf(file.fileUrl, file.originalFile.originalFileName || file.filePath)
+        .catch(error => {
+          console.error('Error during ingestion:', error);
+          toast.error('Failed to ingest the PDF!');
+        })))
+      // .then(() => setLoading(false));
+  };
+
+  // Function to ingest a PDF file:
   async function ingestPdf(fileUrl: string, fileName: string) {
     try {
       const response = await axios.post('/api/ingestPdf', {
@@ -116,6 +129,9 @@ export default function Dashboard({
     } catch (error) {
       console.error('Error deleting document', error);
       toast.error('Failed to delete the document!');
+    } finally {
+      // Clear toast notifications as they pop up one after another:
+      toast.clearWaitingQueue();
     }
   };
 
@@ -132,6 +148,7 @@ export default function Dashboard({
         draggable
         pauseOnHover={false}
         theme="dark"
+        limit={1}
       />
       {docsList.length > 0 && (
         <div className="flex flex-col gap-4 mx-10 my-5">
@@ -139,14 +156,14 @@ export default function Dashboard({
             {docsList.map((doc: any) => (
               <div
                 key={doc.id}
-                className="flex justify-between border border-black shadow-lg bg-ancient-beige p-3 hover:bg-gray-100 transition sm:flex-row flex-col sm:gap-0 gap-3 rounded-xl "
+                className="flex justify-between border border-black shadow-lg bg-ancient-beige p-3 hover:bg-gray-100 transition sm:flex-row flex-col sm:gap-0 gap-3 rounded-xl"
               >
                 <button
                   onClick={() => router.push(`/document/${doc.id}`)}
                   className="flex gap-4"
                 >
                   <DocumentIcon />
-                  <span>{doc.fileName}</span>
+                  <span className="truncate-text">{doc.fileName}</span>
                 </button>
                 <div className="flex gap-4 items-center">  
                 <span>{formatDistanceToNow(doc.createdAt)} ago</span>
@@ -182,7 +199,13 @@ export default function Dashboard({
             Ingesting your PDF...
           </button>
         ) : (
-          <UploadDropZone />
+          <UploadButton options={options} onComplete={handleComplete}>
+            {({ onClick }) => (
+              <button onClick={onClick} disabled={loading} className="flex justify-center bg-ancient-beige border border-black rounded-xl py-2 px-4 hover:bg-gray-100 transition">
+                Upload a file...
+              </button>
+            )}
+          </UploadButton>
         )}
       </div>
     </div>
