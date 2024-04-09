d02315ddf083af91a17ea6d56e19cc4102cc33f4
DavidGir <davidgiroux149@gmail.com>
2024-04-09 14:17:33 -0400

    Update LoadingAnimation component to use lottie-react and animation1.json

--- components/ui/LoadingAnimation.tsx
+++ components/ui/LoadingAnimation.tsx
@@ -1,31 +1,12 @@
 'use client'
 
-import React, { useState } from 'react';
-import Lottie from 'react-lottie';
+import React from 'react';
+import Lottie from 'lottie-react';
+import animationData from '../../public/animation1.json';
 
 const LoadingAnimation = () => {
-  const [animationState] = useState({
-    isStopped: false,
-    isPaused: false,
-  });
-
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: require('../../public/animation1.json'),
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
-
   return (
-    <Lottie
-      options={defaultOptions}
-      height={400}
-      width={400}
-      isStopped={animationState.isStopped}
-      isPaused={animationState.isPaused}
-    />
+    <Lottie animationData={animationData} loop={true} autoplay={true} style={{ width: 400, height: 400 }} />
   );
 };
 
