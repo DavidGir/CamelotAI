95bdcde7b62c2d04fdfe1faa7123f753f3f89258
DavidGir <davidgiroux149@gmail.com>
2024-04-09 14:17:39 -0400

    Remove loading component

--- app/dashboard/loading.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingAnimation from '../../components/ui/LoadingAnimation';
-
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-ancient-beige">
-      <LoadingAnimation />
-    </div>
-  );
-}
