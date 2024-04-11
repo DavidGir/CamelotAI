f66b3d5c61a4d9ece8a1c9d50afff43da756a667
DavidGir <davidgiroux149@gmail.com>
2024-04-11 10:01:22 -0400

    Add ToastContainer component for toast notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Lancelot } from "next/font/google";
 import "../styles/global.css";
 import dynamic from "next/dynamic";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 // Using dynamic import as the loading animation will only be loaded and rendered client-side
 // which is useful as it contains client-specific code (like direct DOM manipulations or document objects which are not available on the server).
@@ -46,6 +48,10 @@ export default function RootLayout({
           </ClerkLoading>
           <ClerkLoaded>
             {children}
+            <ToastContainer 
+              limit={1}
+              newestOnTop={false}
+            />
           </ClerkLoaded>
         </body>
       </html>
