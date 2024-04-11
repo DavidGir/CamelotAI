de726dad34f14f8bc73e153a32646df397ff60e4
DavidGir <davidgiroux149@gmail.com>
2024-04-11 11:00:38 -0400

    Add Suspense and Loading component to improve user experience in page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import Dashboard from './Dashboard';
 import prisma from '../utils/prisma';
 import { currentUser } from '@clerk/nextjs';
 import type { User } from '@clerk/nextjs/api';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 export default async function Page() {
   const user: User | null = await currentUser();
@@ -17,7 +19,9 @@ export default async function Page() {
 
   return (
     <div>
-      <Dashboard docsList={docsList} />
+      <Suspense fallback={<Loading />}>
+        <Dashboard docsList={docsList} />
+      </Suspense>
     </div>
   );
 }
