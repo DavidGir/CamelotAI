24b1dc4255ca995712a213d3bb716466e0f684fe
DavidGir <davidgiroux149@gmail.com>
2024-04-11 21:23:46 -0400

    Refactor useLocalStorage hook to support updating state with a function

--- app/hooks/useLocalStorage.ts
+++ app/hooks/useLocalStorage.ts
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
 
-export default function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void] {
-  const [savedValue, setSavedValue] = useState(initialValue);
+export default function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val: T) => T)) => void] {
+  const [savedValue, setSavedValue] = useState<T>(initialValue);
 
   useEffect(() => {
     const item = localStorage.getItem(key);
     if (item) setSavedValue(JSON.parse(item));
   }, [key]);
 
-  const updateValue = (value: T) => {
-    setSavedValue(value);
-    localStorage.setItem(key, JSON.stringify(value));
+  const updateValue = (value: T | ((val: T) => T)) => {
+    const newValue = typeof value === 'function' ? (value as (val: T) => T)(savedValue) : value;
+    setSavedValue(newValue);
+    localStorage.setItem(key, JSON.stringify(newValue));
   };
+
   return [savedValue, updateValue];
 };
