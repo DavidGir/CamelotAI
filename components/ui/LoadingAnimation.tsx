a47afe9497b72ddebc104ab407e0e8cf0f84bb15
DavidGir <davidgiroux149@gmail.com>
2024-04-18 15:24:37 -0400

    Update LoadingAnimation.tsx to use gray-spinner.json instead of animation1.json

--- components/ui/LoadingAnimation.tsx
+++ components/ui/LoadingAnimation.tsx
@@ -2,11 +2,11 @@
 
 import React from 'react';
 import Lottie from 'lottie-react';
-import animationData from '../../public/animation1.json';
+import animationData from '@/public/gray-spinner.json';
 
 const LoadingAnimation = () => {
   return (
-    <div className="fixed bg-ancient-beige top-0 left-0 w-full h-full z-50 flex justify-center items-center">
+    <div className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center">
        <Lottie animationData={animationData} loop={true} autoplay={true} style={{ width: 400, height: 400 }} />       
     </div>
   );
